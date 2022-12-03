991e4a1893520deba3d3fb0898b0be10225ef37c
rohitnayal12 <rohitnayalkld@gmail.com>
2022-12-03 17:57:29 +0530

    errors found

--- palindrome.js
+++ palindrome.js
@@ -1,9 +1,9 @@
 let name ="ishan";
 let name2="";
-for(let i=name.length-1;i<=0;i--){
+for(let i=name.length-1;i>=0;i--){
     name2=name2+name[i];    
 }
-if(name2<=name){
+if(name2==name){
   console.log("Yes");
 }else{
   console.log("No");
